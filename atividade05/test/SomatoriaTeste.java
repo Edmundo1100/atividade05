e5934dd7ef1415ac0a44b76b2c5a8028ab82de14
Edmundo1100 <edmundo1100@hotmail.com>
2018-05-15 20:22:27 -0300

    ok falha 02

--- atividade05/test/SomatoriaTeste.java
+++ atividade05/test/SomatoriaTeste.java
@@ -10,29 +10,33 @@ public class SomatoriaTeste {
         MathOps mockedMathOps = mock(MathOps.class);
         when(mockedMathOps.fatorial(5)).thenReturn(120);
         when(mockedMathOps.fatorial(10)).thenReturn(3628800);
-        
+
         Primo mockedPrimo = mock(Primo.class);
         when(mockedPrimo.ehPrimo(5)).thenReturn(Boolean.TRUE);
         when(mockedPrimo.ehPrimo(10)).thenReturn(Boolean.FALSE);
-        
+
         Somatoria soma = new Somatoria(mockedMathOps);
-        int[] numeros = {5,10};
-       
-      
+        int[] numeros = {5, 10};
+
         assertEquals(3628800, soma.somaDeFatoriais(numeros, mockedPrimo));
     }
-    
-   @Test
+
+    @Test
     public void somaDeFatoriais_Vetor02() {
-        MathOps MathOps = null;
+        MathOps mockedMathOps = mock(MathOps.class);
+        when(mockedMathOps.fatorial(3)).thenReturn(6);
+        when(mockedMathOps.fatorial(4)).thenReturn(24);
+        when(mockedMathOps.fatorial(5)).thenReturn(120);
+
+        Somatoria soma = new Somatoria(mockedMathOps);
+        int[] numeros = {3, 4, 4, 5};
         
+        Primo mockedPrimo = mock(Primo.class);
+        when(mockedPrimo.ehPrimo(3)).thenReturn(Boolean.TRUE);
+        when(mockedPrimo.ehPrimo(4)).thenReturn(Boolean.FALSE);
+        when(mockedPrimo.ehPrimo(5)).thenReturn(Boolean.TRUE);
         
-        Somatoria soma = new Somatoria(MathOps);
-        int[] numeros = {3,4,4,5};
-        Primo Primo = null;
-       
-      
-        assertEquals(49, soma.somaDeFatoriais(numeros, Primo));
+        assertEquals(48, soma.somaDeFatoriais(numeros, mockedPrimo));
     }
-    
+
 }
